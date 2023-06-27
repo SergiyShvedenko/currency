de957af016912a80e8554eb02486ed3826aef18a
SergiyShvedenko <127552339+SergiyShvedenko@users.noreply.github.com>
2023-06-27 22:45:24 +0100

    init comit

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import rate_list, status_code
+from currency.views import rate_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('rate/list/', rate_list),
-
-    path('sc1/', status_code)
 ]
